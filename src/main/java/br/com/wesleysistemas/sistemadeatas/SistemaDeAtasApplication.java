6586d51aaf235b5af81c16d2f5e72aac48942d05
oWesleyRT <owesleyrt@gmail.com>
2023-06-28 12:14:26 -0300

    primeiro commit

---
+++ src/main/java/br/com/wesleysistemas/sistemadeatas/SistemaDeAtasApplication.java
@@ -0,0 +1,13 @@
+package br.com.wesleysistemas.sistemadeatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAtasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAtasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wesleysistemas/sistemadeatas/SistemaDeAtasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wesleysistemas.sistemadeatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeAtasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
